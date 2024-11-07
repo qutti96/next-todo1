fc6171ec254be64a6d5db9a1f37d1d0724f5b0d2
qutti96 <qutti96@gmail.com>
2024-11-07 14:53:13 +0900

    Todo一覧ページにタイトル修正

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <p>Todos Page</p>;
+export const metadata: Metadata = {
+  title: 'Todo一覧とフィルター',
+};
+
+export default function TodoListPage() {
+  return (
+    <>
+          <h1 className="font-bold mb-6 text-3xl text-center">Todo一覧とフィルター</h1>
+          <p>TODO作成ボタン(フィルター/ソート)</p>
+    </>
+  );
 }
